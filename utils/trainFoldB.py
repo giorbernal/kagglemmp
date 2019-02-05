5c1db7280de69ed01427d484d285454168829878
Jorge Bernal Chaves <restless_project@hotmail.es>
2019-02-06 00:13:34 +0100

    fixing bugs

--- utils/trainFoldB.py
+++ utils/trainFoldB.py
@@ -8,17 +8,16 @@ from ModelDSBase import ModelDSBaseWrapper
 from AdaBoostClassificationDSBase import AdaBoostClassificationDSBaseModelParamsToMap
 from AdaBoostClassificationDSBase import AdaBoostClassificationDSBaseModel
 
-def train_test(fold_id, df, columns_to_partition):
+def train_test(fold_id, df):
 	print('Initiating training of fold ' + str(fold_id) + ' ...')
 	print('  size: ' + str(df.shape))
-	print('  columns_to_partition: ' + str(columns_to_partition))
 	out_path = 'models/fold' + str(fold_id) + "/test.sav.npy"
 	np.save(out_path,np.array(['pepo','te','migo','micci']))
 	print('Training of fold ' + str(fold_id) + ' finalized!')
 	return (0.6455, np.zeros((2,3)))
 
 
-def train(fold_id, df, columns_to_partition):
+def train(fold_id, df):
 
 	print('Initiating training of fold ' + str(fold_id) + ' ...')
 
@@ -38,13 +37,18 @@ def train(fold_id, df, columns_to_partition):
 	lcabc = abc.getLearningCurves()
 	score = abc.getScore()
 
-	# Save columns partitioned at this fold
-	for c in columns_to_partition:
-		np.save(out_path + '/' + str(c) + '.sav.npy',df[c].unique())
-
 	# Save model
 	abc.save(out_path)
 
 	print('Training of fold ' + str(fold_id) + ' finalized!')
 
 	return (score,lcabc)
+
+def saveColumnsToPartition(fold_id, df, columns_to_partition):
+
+	out_path = 'models/fold' + str(fold_id)
+
+	# Save columns partitioned at this fold
+	for c in columns_to_partition:
+		np.save(out_path + '/' + str(c) + '.sav.npy',df[c].unique())
+
